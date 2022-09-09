94ae84a1bad264d6af2e44dd45c400bc81e0f19f
alexandru-narcis.beg@datasentics.com <alexandru-narcis.beg@datasentics.com>
2022-09-09 13:24:09 +0000

    end of day

--- 08 - Delta Live Tables/DE 8.2 DLT Lab/DS-lab8.py
+++ 08 - Delta Live Tables/DE 8.2 DLT Lab/DS-lab8.py
@@ -9,3 +9,9 @@
 
 # MAGIC %md
 # MAGIC **Your answers**
+# MAGIC #### What are Delta Live Tables (DLT)?
+# MAGIC Delta Live Tables is a framework for building reliable, maintainable, and testable data processing pipelines.
+# MAGIC #### What are advantages of DLT?
+# MAGIC Data monitoring, faster scaling, and built in quality control
+# MAGIC #### You want to create delta live table. What command you use?  
+# MAGIC CREATE OR REFRESH STREAMING LIVE TABLE table_name
