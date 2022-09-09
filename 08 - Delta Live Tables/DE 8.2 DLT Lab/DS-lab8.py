a3f4f7b54e3bfaa207b6565e5568e2e88f8337f5
cristian-eduard.oltean@datasentics.com <cristian-eduard.oltean@datasentics.com>
2022-09-09 14:28:07 +0000

    commit

--- 08 - Delta Live Tables/DE 8.2 DLT Lab/DS-lab8.py
+++ 08 - Delta Live Tables/DE 8.2 DLT Lab/DS-lab8.py
@@ -9,3 +9,9 @@
 
 # MAGIC %md
 # MAGIC **Your answers**
+# MAGIC 
+# MAGIC 1. delta live tables is a framework for building reliable, maintainable, and testable data processing pipelines. You define the transformations to perform on your data, and Delta Live Tables manages task orchestration, cluster management, monitoring, data quality, and error handling
+# MAGIC 
+# MAGIC 2. benefits of DLT include support of both streaming and batch data processing with a single API, monitoring of pipelines with the DLT user interface, and chain dependency of dataframes that automatically propagates any updates to the pipeline downstream.
+# MAGIC 
+# MAGIC 3. we create it from the ui of databricks
