a25f3416160cc72da1b89be7d65be2700930a275
daniela-gabriela.vlasceanu@datasentics.com <daniela-gabriela.vlasceanu@datasentics.com>
2022-09-12 08:14:08 +0000

    11
    done

--- 11 - Managing Permissions/DS-lab11.py
+++ 11 - Managing Permissions/DS-lab11.py
@@ -14,11 +14,14 @@
 # MAGIC 
 # MAGIC **Your Answers**  
 # MAGIC - **What is Unity Catalog?**  
-# MAGIC 
+# MAGIC Unity Catalog is a unified governance solution for all data and AI assets including files, tables, machine learning models and dashboards in your lakehouse on any cloud.
 # MAGIC - **What is order in three-layer namespace with Unity Catalog?**  
-# MAGIC 
+# MAGIC Catalogs, schemas, and tables
 # MAGIC - **You are an administrator and you want to give ability to add, delete, and modify data to or from an object to user john.doe@company.com on the table named cars. What command you use?**  
-# MAGIC 
+# MAGIC GRANT ADD ON cars TO john.doe@company.com  
+# MAGIC GRANT DELETE ON cars TO john.doe@company.com  
+# MAGIC GRANT MODIFY ON cars TO john.doe@company.com  
 # MAGIC - **You are an administrator and you want to give ability read access to an object to user john.doe@company.com on the table named cars. What command you use?**  
-# MAGIC 
+# MAGIC GRANT READ ON cars TO john.doe@company.com  
 # MAGIC - **You are an administrator and you want to give gives all privileges to user john.doe@company.com on the table named cars. What command you use?**  
+# MAGIC GRANT ALL PRIVILEGES ON cars TO john.doe@company.com
