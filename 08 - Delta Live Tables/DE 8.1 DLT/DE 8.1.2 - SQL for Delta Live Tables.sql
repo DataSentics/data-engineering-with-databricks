0979a8f441dbfc15f9ec7c39a657b0b4b9bebd45
fabian-augustin-ilie.radomir@datasentics.com <fabian-augustin-ilie.radomir@datasentics.com>
2022-09-15 13:30:54 +0000

    Finished up to 8.1

--- 08 - Delta Live Tables/DE 8.1 DLT/DE 8.1.2 - SQL for Delta Live Tables.sql
+++ 08 - Delta Live Tables/DE 8.1 DLT/DE 8.1.2 - SQL for Delta Live Tables.sql
@@ -183,17 +183,21 @@ AS
 -- COMMAND ----------
 
 -- TODO
--- CREATE OR REFRESH LIVE TABLE sales_order_in_chicago
--- COMMENT "Sales orders in Chicago."
--- AS
---   SELECT city, order_date, customer_id, customer_name, ordered_products_explode.curr, 
---          sum(ordered_products_explode.price) as sales, 
---          sum(ordered_products_explode.qty) as quantity, 
---          count(ordered_products_explode.id) as product_count
---   FROM (SELECT city, order_date, customer_id, customer_name, explode(ordered_products) as ordered_products_explode
---         FROM sales_orders_cleaned 
---         WHERE city = 'Chicago')
---   GROUP BY order_date, city, customer_id, customer_name, ordered_products_explode.curr
+
+-- sales_orders_cleaned will not work. To use that table, we must write "FROM LIVE.sales_orders_cleaned"
+-- That is because "sales_orders_cleaned" is a DLT table, and in order to be referenced the LIVE prefix must be used
+
+CREATE OR REFRESH LIVE TABLE sales_order_in_chicago
+COMMENT "Sales orders in Chicago."
+AS
+  SELECT city, order_date, customer_id, customer_name, ordered_products_explode.curr, 
+         sum(ordered_products_explode.price) as sales, 
+         sum(ordered_products_explode.qty) as quantity, 
+         count(ordered_products_explode.id) as product_count
+  FROM (SELECT city, order_date, customer_id, customer_name, explode(ordered_products) as ordered_products_explode
+        FROM LIVE.sales_orders_cleaned 
+        WHERE city = 'Chicago')
+  GROUP BY order_date, city, customer_id, customer_name, ordered_products_explode.curr
 
 -- COMMAND ----------
 
