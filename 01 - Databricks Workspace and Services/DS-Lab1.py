39dd0ea39d0d900d8d471957a130a435b0b3f2e4
ovidiutoma1 <ovidiutoma1>
2022-12-13 15:07:35 +0000

    DS Lab 1 - First 3 questions answered

--- 01 - Databricks Workspace and Services/DS-Lab1.py
+++ 01 - Databricks Workspace and Services/DS-Lab1.py
@@ -11,7 +11,16 @@
 # COMMAND ----------
 
 # MAGIC %md 
-# MAGIC **Your answers about DWH**
+# MAGIC - Data warehousing is storing data in a data warehouse (basically a storage of structured data)
+# MAGIC - Main use case is for storing data coming from relational databases
+# MAGIC - Advantages:
+# MAGIC   - Changes are tracked, so one can check the changes over time
+# MAGIC   - Data is never over-written
+# MAGIC - Disadvantages:
+# MAGIC   - Lack of unstructured data support
+# MAGIC   - Usually built on-premise - meaning high initial and maintenance cost, as well as a difficulty to migrate to more updated technologies such as the cloud
+# MAGIC   - Difficulty to provide data for multiple case uses due to the nature of the ETL process
+# MAGIC - The data modeling approach is dimensional modeling, through the star schema or OLAP (online analytical processing) cubes. Basically fact and dimension based.
 
 # COMMAND ----------
 
@@ -27,7 +36,18 @@
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC **Your answers about Data Lake**
+# MAGIC - A data lake is a storage that can hold any type of data, structured, semi-structured or unstructured.
+# MAGIC - Main use case is for storing and processing "big data" specific data, such as images, videos, audio, but can also be used for classic structured data.
+# MAGIC - Advantages:
+# MAGIC   - Posibility to store and process any kind of data.
+# MAGIC   - The raw data can be considered as a single source of truth.
+# MAGIC   - Availibility to whole company, and ease of access.
+# MAGIC   - The possibility to be used by data scientists and data analysts as well as data engineers.
+# MAGIC   - Simple billing process.
+# MAGIC - Disadvantages:
+# MAGIC   - While the raw data is the single source of truth, the data manipulation results depend on how the users actually manipulate it. This can lead to incorrect results.
+# MAGIC   - The lack of governance can lead to a storage mess.
+# MAGIC - Rather than using the normalization principles, which are very efficient in a data warehouse, the data lakes are largely based on gathering the facts and dimensions together, and partitioning the data for reading only the specific information needed instead of having to go through all the data each time.
 
 # COMMAND ----------
 
@@ -45,7 +65,16 @@
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC **Your answers about Data LakeHouse**
+# MAGIC - A data lakehouse is basically a platform that brings together the power of computing with data storage.
+# MAGIC - Main use is to actually compute over the data through a unified platform.
+# MAGIC - Advantages:
+# MAGIC   - Unified platform that combines all the elements that a data engineer, a data analyst and a data scientist would use (git/repository integration, data lake storage, computing, the use of Delta Lake).
+# MAGIC   - Delta Lake usage, which brings ACID transactions, indexing (for faster computing), and governance (unity catalog).
+# MAGIC - Disadvantages:
+# MAGIC   - Depending on the type of work needed, a data lakehouse could be more than needed.
+# MAGIC - It can use both the classic data modeling approach used by data warehouses, and the data model used by data lakes, depending on the specific data types that need to be worked on.
+# MAGIC - Best format to use would be the Delta format, as it brings the added layers of ACID transactions and indexing over parquet files.
+# MAGIC - It brings all the functions together under the big umbrella that is the Databricks platform. The ability to access the data stored in a data lake, combined with the ability to do the data manipulations needed, posibility of using git repositories and everything under the Delta Lake element brings the best of all worlds together.
 
 # COMMAND ----------
 
