9252213c343708490bde69b48a3d0bef3f35f21e
alexandrucheciches <alexandrucheciches>
2022-12-13 15:07:36 +0000

    work-on-de-labs-and-DS-Lab1

--- 01 - Databricks Workspace and Services/DS-Lab1.py
+++ 01 - Databricks Workspace and Services/DS-Lab1.py
@@ -12,6 +12,17 @@
 
 # MAGIC %md 
 # MAGIC **Your answers about DWH**
+# MAGIC 
+# MAGIC - DWH is a traditional approach on working with data. It follows an ETL-like approach, meaning data is first collected, then transformed to specific requirements and then loaded into a warehouse
+# MAGIC - The main use case is analytics
+# MAGIC 
+# MAGIC - Advantages
+# MAGIC   - All the changes to data are tracked, it provides and integrated system for organizations and their data, provides realiable and quality data
+# MAGIC 
+# MAGIC - Disadvantages
+# MAGIC   - No support for unrelational data, primarily based on on-premise architecture making it costly to maintain
+# MAGIC 
+# MAGIC - Dimensional modelling which uses the concepts of facts and dimensions, usually found in OLAP systems.
 
 # COMMAND ----------
 
@@ -28,6 +39,21 @@
 
 # MAGIC %md
 # MAGIC **Your answers about Data Lake**
+# MAGIC 
+# MAGIC - Centralized repository that accepts all kinds of data, structured, unstructured and semi-structured which is stored in its original state before being transformed for specific use-cases.
+# MAGIC - Big Data applications
+# MAGIC 
+# MAGIC - Advantages
+# MAGIC   - Data can be considered as a single source of truth 
+# MAGIC   - Stored data can be multi-structured and from diverse sources 
+# MAGIC   - Broad use-cases since every data practitioners can acces raw data and tailor it to their specific needs
+# MAGIC - Disadvantages
+# MAGIC   - Since practitioners have access to data in its raw state, each user might choose a different approach and come to a different result when working with it
+# MAGIC   - Time lost since everyone needs to do their transforming of the data
+# MAGIC   - No support for data governance
+# MAGIC 
+# MAGIC - Isn't as normalized and is driven by partitions
+# MAGIC   
 
 # COMMAND ----------
 
@@ -46,6 +72,24 @@
 
 # MAGIC %md
 # MAGIC **Your answers about Data LakeHouse**
+# MAGIC 
+# MAGIC - A unified platform for almost every use case for working with data
+# MAGIC 
+# MAGIC - Since it provides a replacement for the dependancies between data warehouses and data lakes it has a very broad area of aplication, but it is mainly used for ml and data science applications on cloud
+# MAGIC 
+# MAGIC - Advantages
+# MAGIC   - Makes working on data easily colaborative between data practitioners
+# MAGIC   - Open-source standards and formats
+# MAGIC   - Easily scalable
+# MAGIC 
+# MAGIC - Disadvantages
+# MAGIC   - Can be overkill for some use cases
+# MAGIC   
+# MAGIC - Dependant on the specific use case and tools used
+# MAGIC 
+# MAGIC - Delta format which is used because it provides acid transcations, faster data processing and governance to existing data formats (e.g parquet) and can store any type of data
+# MAGIC 
+# MAGIC - The Databricks Lakehouse takes the best from both worlds (data warehouses, data lakes) and combines them into a platform to enable any type of work revolving around data. It also facilitates keeping data in cloud storage in open source data fromats.
 
 # COMMAND ----------
 
@@ -56,6 +100,8 @@
 
 # MAGIC %md
 # MAGIC **Your answers about problems with data**
+# MAGIC 
+# MAGIC - 
 
 # COMMAND ----------
 
