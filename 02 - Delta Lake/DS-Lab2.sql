2620f0359922f6e58825346b2ea3881fb1adb6a3
ovidiutoma1 <ovidiutoma1>
2022-12-14 14:31:22 +0000

    DS Lab 2 - Finished

--- 02 - Delta Lake/DS-Lab2.sql
+++ 02 - Delta Lake/DS-Lab2.sql
@@ -10,7 +10,14 @@
 -- COMMAND ----------
 
 -- MAGIC %md
--- MAGIC **Your answers about Delta Lake**
+-- MAGIC - Delta Lake is a type of storage layer that is the pillar of the Databricks Lakehouse platform. It is an open-source project.
+-- MAGIC - Advantages:
+-- MAGIC   - Uses ACID transactions
+-- MAGIC   - Indexing can speed up querying up to 48x
+-- MAGIC   - Brings governance to the data lake
+-- MAGIC - ACID means Atomicity, Consistency, Isolation and Durability - these are all needed in order to have the guarantee of proper and reliable transactions.
+-- MAGIC - It is not proprietary software, or any kind of actual storage.
+-- MAGIC - Delta Lake is built on top of standard data formats, mainly the parquet file format.
 
 -- COMMAND ----------
 
@@ -26,6 +33,14 @@
 -- COMMAND ----------
 
 -- Create the table here
+CREATE TABLE company
+(name STRING, companyId INT, income DOUBLE);
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC - The table is managed - this is the default setting, along with the default Delta format
+-- MAGIC - By default, the table is stored in the root storage location configured by the admin. The location can however be specified at the catalog or schema level. **WOULD LIKE TO GO OVER THIS TOGETHER, AS IT'S A BIT UNCLEAR TO ME**
 
 -- COMMAND ----------
 
@@ -41,10 +56,16 @@
 -- COMMAND ----------
 
 -- INSERT 3 records in single transaction
+INSERT INTO company VALUES
+("John", 1, 15000),
+("Mark", 2, 12500.60),
+("Emily", 3, 11200);
 
 -- COMMAND ----------
 
 -- INSERT 2 records each in a different transaction
+INSERT INTO company VALUES ("Bridget", 4, 11000);
+INSERT INTO company VALUES ("Jonathan", 5, 10000.5);
 
 -- COMMAND ----------
 
@@ -61,6 +82,21 @@
 -- COMMAND ----------
 
 -- Query/View your table here
+SELECT * FROM company
+
+-- If in another database:
+-- SELECT * FROM databaseX.company;
+
+-- COMMAND ----------
+
+DESCRIBE HISTORY company
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC - Delta Lake uses a transaction log in order to store the version history. With each transaction, a new version is created.
+-- MAGIC - A user that reads while another writes in a table will see the latest commited version of the table. It will not interrupt any transactions.
+-- MAGIC - There are no locks on reading or writing - there can be as many reads as needed - **NEED TO CONFIRM THIS**
 
 -- COMMAND ----------
 
@@ -77,6 +113,37 @@
 -- COMMAND ----------
 
 -- Update your table here
+UPDATE company
+SET name = "Britney"
+WHERE name LIKE "B%"
+
+-- COMMAND ----------
+
+UPDATE company
+SET name = "Jonny"
+WHERE name LIKE "J%"
+
+-- COMMAND ----------
+
+UPDATE company
+SET COLUMN needs_raise
+
+-- COMMAND ----------
+
+DESCRIBE HISTORY company
+
+-- COMMAND ----------
+
+SELECT * FROM company
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC - One transaction has been triggered per UPDATE command (regardless of the number of records changed).
+-- MAGIC - As per the Databricks documentation, for managing concurrent transactions, Databricks uses optimistic concurrency control. This means that there are no locks on reading or writing against a table, and deadlock is not a possibility.
+-- MAGIC - Updated records can be seen immediately after writing (a new read command is needed though).
+-- MAGIC - If the job fails mid-way, the transaction will not take place.
+-- MAGIC - A new version is created with each successful transaction. 
 
 -- COMMAND ----------
 
@@ -92,6 +159,20 @@
 -- COMMAND ----------
 
 -- Delete your table here
+DELETE FROM company
+WHERE name = "Jonny" AND income > 10000
+
+-- COMMAND ----------
+
+DESCRIBE HISTORY company
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC - One transaction has been triggered per DELETE command (regardless of the number of records changed).
+-- MAGIC - As per the Databricks documentation, for managing concurrent transactions, Databricks uses optimistic concurrency control. This means that there are no locks on reading or writing against a table, and deadlock is not a possibility. This applies for deletions as well.
+-- MAGIC - Updated records can be seen immediately after deleting (a new read command is needed though).
+-- MAGIC - A new version is created with each successful transaction. 
 
 -- COMMAND ----------
 
@@ -110,11 +191,35 @@ CREATE OR REPLACE TEMP VIEW updates(name, companyId, income, type) AS VALUES
   ("Omar", 13, 24.5, "update"),
   ("", 8, 12.3, "delete"),
   ("Blue", 7, 123.5, "insert"),
-  ("Diya", 15, 43.2, "update");
+  ("Diya", 4, 43.2, "update");
 
 -- COMMAND ----------
 
 -- Merge the above updates into company table here
+MERGE INTO company c
+USING updates u
+ON c.companyId = u.companyId
+WHEN MATCHED AND u.type = "update"
+  THEN UPDATE SET *
+WHEN MATCHED AND u.type = "delete"
+  THEN DELETE
+WHEN NOT MATCHED AND u.type = "insert"
+  THEN INSERT *
+
+-- COMMAND ----------
+
+SELECT * FROM company
+
+-- COMMAND ----------
+
+DESCRIBE HISTORY company
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC - The main advantage for using MERGE is that we can only use one larger command, creating only one transaction for different data editing commands in a quicker way.
+-- MAGIC - There needs to be at least one common column.
+-- MAGIC - A merge operation can fail if multiple rows of the source dataset match and the merge attempts to update the same rows of the target Delta table. If the job fails mid-way, the transaction will not take place.
 
 -- COMMAND ----------
 
@@ -129,6 +234,14 @@ CREATE OR REPLACE TEMP VIEW updates(name, companyId, income, type) AS VALUES
 -- COMMAND ----------
 
 -- Delete your table here
+DELETE FROM company
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC - The DELETE FROM statement deletes all table rows which match the condition (if none given, all rows are erased).
+-- MAGIC - One transaction is triggered.
+-- MAGIC - If the job fails mid-way, the transaction will not take place.
 
 -- COMMAND ----------
 
@@ -137,7 +250,7 @@ CREATE OR REPLACE TEMP VIEW updates(name, companyId, income, type) AS VALUES
 
 -- COMMAND ----------
 
-CREATE TABLE students
+CREATE TABLE IF NOT EXISTS students
   (id INT, name STRING, value DOUBLE);
   
 INSERT INTO students VALUES (1, "Yve", 1.0);
@@ -196,6 +309,24 @@ DESCRIBE DETAIL students
 
 -- COMMAND ----------
 
+-- MAGIC %python
+-- MAGIC display(dbutils.fs.ls("dbfs:/user/hive/warehouse/students"))
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC display(dbutils.fs.ls("dbfs:/user/hive/warehouse/students/_delta_log"))
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC - Table students has 4 partitions. This number can be reduced by using the OPTIMIZE command.
+-- MAGIC - Storage location is "dbfs:/user/hive/warehouse/students/"
+-- MAGIC - Information found in the metadata above.
+-- MAGIC - Partitioning in this case is happening due to a number of operations where only one or several records were modified, however this is not needed nor recommended for small to medium file sizes. We could use the PARTITION BY command to partition by certain criteria that we would want to apply.
+
+-- COMMAND ----------
+
 -- MAGIC %md
 -- MAGIC ### History of Delta Table
 -- MAGIC - How can you see the history of the delta table?
@@ -208,10 +339,18 @@ DESCRIBE DETAIL students
 -- COMMAND ----------
 
 -- Run the command to see history of students table
+DESCRIBE HISTORY students
 
 -- COMMAND ----------
 
 -- Roll back to different vesrion in the history of stundets table
+RESTORE TABLE students TO VERSION AS OF 5
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC - History is retained by default for 30 days. This is how long you can go back. This time interval can be changed using "delta.logRetentionDuration = 'interval (interval)'"
+-- MAGIC - The history can be cleaned using the VACUUM command. This command is normally set to be used for at least 7 days, however we can override this (risky).
 
 -- COMMAND ----------
 
@@ -226,6 +365,14 @@ DESCRIBE DETAIL students
 -- COMMAND ----------
 
 -- Run the OPTIMIZE command on the students table
+OPTIMIZE students
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC - This command combines smaller files into bigger ones.
+-- MAGIC - It's useful for improving the speed of read queries from a table.
+-- MAGIC - It could be used whenever we are writing into the tables.
 
 -- COMMAND ----------
 
@@ -242,3 +389,14 @@ DESCRIBE DETAIL students
 -- COMMAND ----------
 
 -- Run the ZORDER command
+OPTIMIZE students
+ZORDER BY id
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC - The ZORDER command colocates column information in the same set of files. This speeds up data retrieval when filtering on provided fields by colocating data with similar values within data files.
+-- MAGIC - It should be used on larger sets of data.
+-- MAGIC - It should be run depending on how much data is being added.
+-- MAGIC - It works best when used on columns that have high number of distinct values. It can be used on several columns, however the speed/effectiveness goes down with each column added.
+-- MAGIC - **Not sure if whenever we use the command it applies to the column we mention at that point, or if this is an additive command**
